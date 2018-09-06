7980672b7aec449589354ad322389a341cd33df2
Ting <ting@rytass.com>
2018-09-06 19:30:48 +0800

    type Section

--- PK10FiveStars/components/TypeSection.jsx
+++ PK10FiveStars/components/TypeSection.jsx
@@ -22,6 +22,20 @@ const styles = {
     border: '1px solid #4a4a4a',
     margin: '4px 8px 8px 8px',
   },
+  main: {
+    alignSelf: 'stretch',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  fieldWrapper: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
   lineWrapper: {
     alignSelf: 'stretch',
     display: 'flex',
@@ -62,7 +76,7 @@ const styles = {
     backgroundColor: '#f2f2f2',
     fontSize: 13,
     letterSpacing: 1,
-    margin: '0 2px',
+    margin: '0 2px 0 20px',
     color: '#4a4a4a',
     ':hover': {
       backgroundColor: '#eaeaea',
@@ -111,52 +125,270 @@ class TypeSection extends PureComponent<Props> {
         <div style={styles.lineWrapper}>
           <span style={styles.title}>【型态个数】</span>
         </div>
-        <div style={styles.lineWrapper}>
-          <span style={styles.title}>大号数</span>
-          <Field
-            name="bigTypeZero"
-            text="0"
-            buttonstyle={styles.button}
-            component={BooleanButton} />
-          <Field
-            name="bigTypeOne"
-            text="1"
-            buttonstyle={styles.button}
-            component={BooleanButton} />
-          <Field
-            name="bigTypeTwo"
-            text="2"
-            buttonstyle={styles.button}
-            component={BooleanButton} />
-          <Field
-            name="bigTypeThree"
-            text="3"
-            buttonstyle={styles.button}
-            component={BooleanButton} />
-          <Field
-            name="bigTypeFour"
-            text="4"
-            buttonstyle={styles.button}
-            component={BooleanButton} />
-          <Field
-            name="bigTypeFive"
-            text="5"
-            buttonstyle={styles.button}
-            component={BooleanButton} />
-          <button
-            type="button"
-            key="all"
-            onClick={() => this.allValue('bigType')}
-            style={styles.allBtn}>
-            全
-          </button>
-          <button
-            type="button"
-            key="clear"
-            onClick={() => this.clearValue('bigType')}
-            style={styles.clearBtn}>
-            清
-          </button>
+        <div style={styles.main}>
+          <div style={styles.fieldWrapper}>
+            <div style={styles.lineWrapper}>
+              <span style={styles.title}>大号数</span>
+              <Field
+                name="bigTypeZero"
+                text="0"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="bigTypeOne"
+                text="1"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="bigTypeTwo"
+                text="2"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="bigTypeThree"
+                text="3"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="bigTypeFour"
+                text="4"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="bigTypeFive"
+                text="5"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <button
+                type="button"
+                key="bigAll"
+                onClick={() => this.allValue('bigType')}
+                style={styles.allBtn}>
+                全
+              </button>
+              <button
+                type="button"
+                key="bigClear"
+                onClick={() => this.clearValue('bigType')}
+                style={styles.clearBtn}>
+                清
+              </button>
+            </div>
+            <div style={styles.lineWrapper}>
+              <span style={styles.title}>奇号数</span>
+              <Field
+                name="oddTypeZero"
+                text="0"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oddTypeOne"
+                text="1"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oddTypeTwo"
+                text="2"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oddTypeThree"
+                text="3"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oddTypeFour"
+                text="4"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oddTypeFive"
+                text="5"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <button
+                type="button"
+                key="oddAll"
+                onClick={() => this.allValue('oddType')}
+                style={styles.allBtn}>
+                全
+              </button>
+              <button
+                type="button"
+                key="oddClear"
+                onClick={() => this.clearValue('oddType')}
+                style={styles.clearBtn}>
+                清
+              </button>
+            </div>
+            <div style={styles.lineWrapper}>
+              <span style={styles.title}>质号数</span>
+              <Field
+                name="primeTypeZero"
+                text="0"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="primeTypeOne"
+                text="1"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="primeTypeTwo"
+                text="2"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="primeTypeThree"
+                text="3"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="primeTypeFour"
+                text="4"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="primeTypeFive"
+                text="5"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <button
+                type="button"
+                key="primeAll"
+                onClick={() => this.allValue('primeType')}
+                style={styles.allBtn}>
+                全
+              </button>
+              <button
+                type="button"
+                key="primeClear"
+                onClick={() => this.clearValue('primeType')}
+                style={styles.clearBtn}>
+                清
+              </button>
+            </div>
+          </div>
+          <div style={styles.fieldWrapper}>
+            <div style={styles.lineWrapper}>
+              <span style={styles.title}>0路个数</span>
+              <Field
+                name="zeroLoadTypeZero"
+                text="0"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="zeroLoadTypeOne"
+                text="1"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="zeroLoadTypeTwo"
+                text="2"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="zeroLoadTypeThree"
+                text="3"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <div style={styles.button} />
+              <button
+                type="button"
+                key="zeroLoadAll"
+                onClick={() => this.allValue('zeroLoadType')}
+                style={styles.allBtn}>
+                全
+              </button>
+              <button
+                type="button"
+                key="zeroLoadClear"
+                onClick={() => this.clearValue('zeroLoadType')}
+                style={styles.clearBtn}>
+                清
+              </button>
+            </div>
+            <div style={styles.lineWrapper}>
+              <span style={styles.title}>1路个数</span>
+              <Field
+                name="oneLoadTypeZero"
+                text="0"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oneLoadTypeOne"
+                text="1"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oneLoadTypeTwo"
+                text="2"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oneLoadTypeThree"
+                text="3"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="oneLoadTypeFour"
+                text="4"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <button
+                type="button"
+                key="oneLoadAll"
+                onClick={() => this.allValue('oneLoadType')}
+                style={styles.allBtn}>
+                全
+              </button>
+              <button
+                type="button"
+                key="oneLoadClear"
+                onClick={() => this.clearValue('oneLoadType')}
+                style={styles.clearBtn}>
+                清
+              </button>
+            </div>
+            <div style={styles.lineWrapper}>
+              <span style={styles.title}>2路个数</span>
+              <Field
+                name="twoLoadTypeZero"
+                text="0"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="twoLoadTypeOne"
+                text="1"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="twoLoadTypeTwo"
+                text="2"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <Field
+                name="twoLoadTypeThree"
+                text="3"
+                buttonstyle={styles.button}
+                component={BooleanButton} />
+              <div style={styles.button} />
+              <button
+                type="button"
+                key="twoLoadAll"
+                onClick={() => this.allValue('twoLoadType')}
+                style={styles.allBtn}>
+                全
+              </button>
+              <button
+                type="button"
+                key="twoLoadClear"
+                onClick={() => this.clearValue('twoLoadType')}
+                style={styles.clearBtn}>
+                清
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
