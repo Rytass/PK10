cfc7b43019777281f02adca5a02121a3c32ce183
Ting <ting@rytass.com>
2018-09-06 18:50:14 +0800

    set ResultSection in OtherPage

--- PK10FiveStars/containers/OtherPage.jsx
+++ PK10FiveStars/containers/OtherPage.jsx
@@ -1,24 +1,120 @@
 // @flow
 
-import React from 'react';
+import React, { PureComponent } from 'react';
+import {
+  change,
+  reduxForm,
+  formValueSelector,
+} from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import initNumberPool from '../helper/init';
+import ResultSection from '../components/ResultSection';
+import BaseSection from '../components/BaseSection';
+import { MAIN_FORM } from '../shared/form';
+import { NO_RESULT } from '../shared/message';
+import { FILTER_ACTIONS } from '../shared/actions';
+
+const selector = formValueSelector(MAIN_FORM);
 
 const styles = {
   wrapper: {
-    height: '100%',
+    padding: 8,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    position: 'relative',
+  },
+  resultWrapper: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    position: 'relative',
   },
 };
 
-function OtherPage() {
-  return (
-    <div style={styles.wrapper}>
-      <h2>Other!!!</h2>
-    </div>
-  );
+type Props = {
+  handleSubmit: Function,
+  changeResultNumber: Function,
+  failFrom: number,
+  failTo: number,
 }
 
-export default OtherPage;
+class OtherPage extends PureComponent<Props> {
+  static ACTIONS = FILTER_ACTIONS;
+
+  state = {
+    runningOptions: null,
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const {
+      runningOptions,
+    } = this.state;
+
+    if (!prevState.runningOptions && runningOptions) {
+      const {
+        changeResultNumber,
+        failFrom,
+        failTo,
+      } = this.props;
+
+      setTimeout(() => {
+        const result = OtherPage.ACTIONS
+          .reduce((prev, curr) => curr(prev, runningOptions), initNumberPool())
+          .filter(num => (
+            num.killFailedCount >= failFrom && failTo >= num.killFailedCount));
+
+        if (!result.length) {
+          changeResultNumber([NO_RESULT]);
+        } else {
+          changeResultNumber(result);
+        }
+
+        this.setState({ runningOptions: null });
+      }, 0);
+    }
+  }
+
+  submit(options) {
+    this.setState({ runningOptions: options });
+  }
+
+  render() {
+    const {
+      handleSubmit,
+    } = this.props;
+
+    const {
+      runningOptions,
+    } = this.state;
+
+    return (
+      <form style={styles.wrapper} onSubmit={handleSubmit(d => this.submit(d))}>
+        <div style={styles.resultWrapper}>
+          <ResultSection isRunning={!!runningOptions} />
+          <BaseSection />
+        </div>
+      </form>
+    );
+  }
+}
+
+const formHook = reduxForm({
+  form: MAIN_FORM,
+  destroyOnUnmount: false,
+});
+
+const reduxHook = connect(
+  state => ({
+    failFrom: selector(state, 'failFrom'),
+    failTo: selector(state, 'failTo'),
+  }),
+  dispatch => bindActionCreators({
+    changeResultNumber: value => change(MAIN_FORM, 'resultNumber', value),
+  }, dispatch),
+);
+
+export default reduxHook(formHook(OtherPage));
