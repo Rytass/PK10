ff0475780e495d002514a563c5e5680cefaecda6
Ting <ting@rytass.com>
2018-09-10 14:00:16 +0800

    tail kiler

---
+++ PK10FiveStars/helper/tailKiller.js
@@ -0,0 +1,169 @@
+import { parseNumber } from './operator';
+
+export function tailKiller(numbers, options) {
+  const composed = Object.entries(options)
+    .filter(entry => entry[0].match(/tail/) && entry[1] === true);
+
+  if (!composed.length) return numbers;
+
+  composed.forEach(([condition]) => {
+    switch (condition) {
+      case 'tailZero': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 0
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailOne': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 1
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailTwo': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 2
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailThree': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 3
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailFour': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 4
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailFive': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 5
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailSix': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 6
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailSeven': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 7
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailEight': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 8
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      case 'tailNine': {
+        numbers.forEach((num) => {
+          if ((
+            parseNumber(num.num[0])
+            + parseNumber(num.num[1])
+            + parseNumber(num.num[2])
+            + parseNumber(num.num[3])
+            + parseNumber(num.num[4])) % 10 === 9
+          ) {
+            num.killFailed();
+          }
+        });
+        break;
+      }
+
+      default:
+        break;
+    }
+  });
+
+  return numbers;
+}
+
+export default tailKiller;
--- PK10FiveStars/shared/actions.js
+++ PK10FiveStars/shared/actions.js
@@ -1,10 +1,12 @@
 import { acrossKiller } from '../helper/acrossKiller';
+import { tailKiller } from '../helper/tailKiller';
 import { bigSmallKiller } from '../helper/bigSmallKiller';
 import { oddEvenKiller } from '../helper/oddEvenKiller';
 import { primeCompositeKiller } from '../helper/primeCompositeKiller';
 
 export const FILTER_ACTIONS = [
   acrossKiller,
+  tailKiller,
   bigSmallKiller,
   oddEvenKiller,
   primeCompositeKiller,
