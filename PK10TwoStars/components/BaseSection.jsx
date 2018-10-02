cf4973ae3f7baf38ae6d35bc304b471ab39700ee
Ting <ting@rytass.com>
2018-10-02 17:07:47 +0800

    base section

--- PK10TwoStars/components/BaseSection.jsx
+++ PK10TwoStars/components/BaseSection.jsx
@@ -1,19 +1,15 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import { clipboard } from 'electron';
 import radium from 'radium';
 import {
   Field,
   change,
-  formValueSelector,
 } from 'redux-form';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { MAIN_FORM } from '../shared/form';
 import TextArea from './Form/TextArea.jsx';
-
-const selector = formValueSelector(MAIN_FORM);
 
 const styles = {
   wrapper: {
@@ -22,7 +18,7 @@ const styles = {
     display: 'flex',
     alignSelf: 'stretch',
     flexDirection: 'column',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     justifyContent: 'flex-start',
     padding: '22px 8px 12px 8px',
     margin: '0 8px',
@@ -30,98 +26,42 @@ const styles = {
     borderColor: '#4a4a4a',
     borderStyle: 'solid',
   },
+  titleWrapper: {
+    display: 'flex',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: '1px 1px 0 1px',
+    borderColor: '#4a4a4a',
+    borderStyle: 'solid',
+    backgroundColor: '#c5d9f9',
+  },
   title: {
     fontSize: 13,
     letterSpacing: 1,
     color: '#4a4a4a',
-    margin: '0 0 2px 6px',
-  },
-  numberText: {
-    fontSize: 13,
-    letterSpacing: 1,
-    color: '#4a4a4a',
-    margin: '6px 0',
-  },
-  btnWrapper: {
-    display: 'flex',
-    alignSelf: 'stretch',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonsWrapper: {
-    display: 'flex',
-    alignSelf: 'stretch',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  actionBtn: {
-    flex: 1,
-    border: '1px solid #4a4a4a',
-    padding: '6px 8px',
-    margin: '0 1px',
-    backgroundColor: '#f2f2f2',
-    fontSize: 13,
-    letterSpacing: 1,
-    color: '#4a4a4a',
-    ':hover': {
-      backgroundColor: '#eaeaea',
-    },
-    ':active': {
-      boxShadow: '0 1px #666',
-      transform: 'translateY(1px)',
-    },
   },
 };
 
-type Props = {
-  changeField: Function,
-  baseLength: number,
-}
-
-class BaseSection extends PureComponent<Props> {
+class BaseSection extends PureComponent {
   render() {
-    const {
-      changeField,
-      baseLength,
-    } = this.props;
-
     return (
       <div style={styles.wrapper}>
+        <div style={styles.titleWrapper}>
+          <span style={styles.title}>大底号码</span>
+        </div>
         <Field
           name="base"
           placeholder="大底号码，双击清空"
           component={TextArea} />
-        <div style={styles.btnWrapper}>
-          <span style={styles.numberText}>
-            {`${baseLength}注`}
-          </span>
-          <div style={styles.buttonsWrapper}>
-            <button
-              type="button"
-              key="paste"
-              style={styles.actionBtn}
-              onClick={() => changeField(clipboard.readText())}>
-              贴
-            </button>
-            <button
-              type="button"
-              key="clear"
-              style={styles.actionBtn}
-              onClick={() => changeField('')}>
-              清
-            </button>
-          </div>
-        </div>
       </div>
     );
   }
 }
 
 const reduxHook = connect(
-  state => ({
-    baseLength: selector(state, 'base').split(/[\n(\r\n)]/).filter(t => t).length,
+  () => ({
+
   }),
   dispatch => bindActionCreators({
     changeField: value => change(MAIN_FORM, 'base', value),
